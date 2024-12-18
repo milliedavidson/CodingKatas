662aa1c4023c9832d70a2286b1f9b2c153f0984a
millie.davidson <millie.davidson@clear.bank>
2024-12-18 17:39:08 +0000

    Added GenerateInputFile

--- CodingKatas/GenerateInputFile.cs
+++ CodingKatas/GenerateInputFile.cs
@@ -6,10 +6,50 @@
         {
             var accountNumbers = new List<string>
             {
+                "123456789",
+                "345882865",
                 "457508000",
-                "664371495",
-                "86110??36",
             };
+
+            var lines = new List<string>();
+
+            foreach (var accountNumber in accountNumbers)
+            {
+                lines.AddRange(ConvertToOcrFormat(accountNumber));
+                lines.Add("");
+            }
+
+            File.WriteAllLines(filePath, lines);
+        }
+
+        private static List<string> ConvertToOcrFormat(string accountNumber)
+        {
+            var ocrLines = new string[4] { "", "", "", "" };
+
+            var ocrDigits = new Dictionary<char, string[]>
+            {
+                { '0', [" _ ", "| |", "|_|", "   "] },
+                { '1', ["   ", "  |", "  |", "   "] },
+                { '2', [" _ ", " _|", "|_ ", "   "] },
+                { '3', [" _ ", " _|", " _|", "   "] },
+                { '4', ["   ", "|_|", "  |", "   "] },
+                { '5', [" _ ", "|_ ", " _|", "   "] },
+                { '6', [" _ ", "|_ ", "|_|", "   "] },
+                { '7', [" _ ", "  |", "  |", "   "] },
+                { '8', [" _ ", "|_|", "|_|", "   "] },
+                { '9', [" _ ", "|_|", " _|", "   "] },
+                { '?', ["   ", "   ", "   ", "   "] }
+            };
+
+            foreach (var digit in accountNumber)
+            {
+                for (var i = 0; i < 4; i++)
+                {
+                    ocrLines[i] += ocrDigits[digit][i];
+                }
+            }
+
+            return [..ocrLines];
         }
     }
 }
